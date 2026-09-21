3aabadae19822f202e3a1ebededbb4eb80cfafc8
MeetPatel-1803 <patelmohit8379@gmail.com>
2026-09-21 11:47:37 +0530

    feat: remaining migrations.

--- src/database/migrations/1789623301442-AutoMigration.ts
+++ src/database/migrations/1789623301442-AutoMigration.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AutoMigration1789623301442 implements MigrationInterface {
-    name = 'AutoMigration1789623301442'
+  name = 'AutoMigration1789623301442';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_events" RENAME COLUMN "noOfSeatsReserved" TO "noOfSeatsRequired"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_events" RENAME COLUMN "noOfSeatsReserved" TO "noOfSeatsRequired"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_events" RENAME COLUMN "noOfSeatsRequired" TO "noOfSeatsReserved"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_events" RENAME COLUMN "noOfSeatsRequired" TO "noOfSeatsReserved"`,
+    );
+  }
 }
